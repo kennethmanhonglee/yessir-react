d1fa0d670d668148ca8b02ff58af1cfa6b71795f
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-09-09 21:48:02 -0700

    finished phase 3 setup for backend

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+const { restoreUser, requireAuth } = require('../../utils/auth');
+
 const router = express.Router();
 
-router.post('/test', (req, res) => {
-    res.json({ requestBody: req.body });
-});
 
 module.exports = router;
