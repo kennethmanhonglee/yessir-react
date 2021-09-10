d2147fe898cd735d8079db04070a34009c5cd3f8
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-09-10 15:17:48 -0700

    finished signup functionality

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -38,6 +38,21 @@ export const restoreUser_thunk = () => async dispatch => {
     return response;
 };
 
+export const signup_thunk = (user) => async (dispatch) => {
+    const { username, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+        method: "POST",
+        body: JSON.stringify({
+            username,
+            email,
+            password,
+        }),
+    });
+    const data = await response.json();
+    dispatch(setUser_actionCreator(data.user));
+    return response;
+};
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
