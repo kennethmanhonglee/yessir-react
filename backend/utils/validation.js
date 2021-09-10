d7e50c3030f1202d87352d3ed7fae85bf5e982d3
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-09-10 15:04:54 -0700

    showing login form

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -3,7 +3,7 @@ const { validationResult } = require('express-validator');
 const handleValidationErrors = (req, _res, next) => {
     const validationErrors = validationResult(req);
 
-    if (!handleValidationErrors.isEmpty()) {
+    if (!validationErrors.isEmpty()) {
         const errors = validationErrors.array().map((error) => `${error.msg}`)
 
         const err = Error('Bad request.');
