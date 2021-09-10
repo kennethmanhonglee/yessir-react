7540cd10ba94a42109b7015eda0266c606568513
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-09-10 10:48:52 -0700

    finished backend user auth setup

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,4 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/test', (req, res) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('Hello from the test route!');
-})
-
 module.exports = router;
