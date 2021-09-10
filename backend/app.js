1667480dec4bc54f9ab4724b73df44526b8add61
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-09-09 19:23:48 -0700

    added error handling

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize');
 
 // routes
 const routes = require('./routes'); //for debugging
@@ -34,5 +35,35 @@ app.use(csurf({
 
 app.use(routes);
 app.use('/api', apiRouter);
+
+//middleware to create error handler for 404
+app.use((_req, _res, next) => {
+    const err = new Error('The requested resource couldn\'t be found.');
+    err.title = 'Resource Not Found';
+    err.errors = ['The requested resource couldn\'t be found.'];
+    err.status = 404;
+    next(err); //next is called with express error handler
+});
+
+//sequelize errors handler
+app.use((err, _req, _res, next) => {
+    // check if error is sequelize error
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message);
+    }
+    next(err);
+});
+
+//generic error handler
+app.use((err, _req, res, next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({ //actually send back error
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
+    });
+});
 
 module.exports = app;
