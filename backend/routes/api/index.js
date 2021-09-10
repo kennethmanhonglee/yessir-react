4e7681973b1440974ae994e073d55a4d366ba66c
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-09-10 08:29:04 -0700

    almost done with login

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,6 +5,15 @@ const { User } = require('../../db/models');
 const { restoreUser, requireAuth } = require('../../utils/auth');
 
 const router = express.Router();
+const sessionRouter = require('./session');
+const usersRouter = require('./users');
 
+
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+    res.json({ requestBody: req.body });
+});
 
 module.exports = router;
