b7afd4b48cf20db859554d18da7d5be2003450dd
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-09-11 10:25:52 -0700

    added structure to splash page

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,15 +1,25 @@
 import { Link } from "react-router-dom";
 import styles from './Home.module.css';
+import { FaSearch } from 'react-icons/fa'
 
 
 const Home = () => {
     return (
         <>
             <div className={styles.main}>
-                <Link to='/'>YESSIR!</Link> { /* placeholder link, change to logo later */}
-                <div className={styles.searchBar}>searchBar</div>
+                <div className={styles.linkIcon}>
+                    <Link to='/'>YESSIR!</Link> { /* placeholder link, change to logo later */}
+                </div>
+                <div className={styles.searchBar}>
+                    <FaSearch style={{ color: 'grey' }} />
+                    <input type='text' placeholder='Find your next spot' />
+                </div>
             </div>
-            <div className='footer'>footer</div>
+            <div className={styles.footer}>
+                <div className={styles.about}>
+                    Github
+                </div>
+            </div>
         </>
     );
 }
