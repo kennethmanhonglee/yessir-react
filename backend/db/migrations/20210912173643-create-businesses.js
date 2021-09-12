5cc40e473ca8b8c675aeafe8df40ed07f9aeafa0
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-09-12 10:40:49 -0700

    created Businesses table

---
+++ backend/db/migrations/20210912173643-create-businesses.js
@@ -0,0 +1,61 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Businesses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      ownerId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      address: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      city: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      state: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      zipCode: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      latitude: {
+        type: Sequelize.DECIMAL,
+        allowNull: false
+      },
+      longitude: {
+        type: Sequelize.DECIMAL,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Businesses');
+  }
+};
---
+++ backend/db/models/businesses.js
@@ -0,0 +1,46 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Businesses = sequelize.define('Businesses', {
+    ownerId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    zipCode: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    latitude: {
+      type: DataTypes.DECIMAL,
+      allowNull: false
+    },
+    longitude: {
+      type: DataTypes.DECIMAL,
+      allowNull: false
+    },
+  }, {});
+  Businesses.associate = function (models) {
+    // associations can be defined here
+  };
+  return Businesses;
+};
