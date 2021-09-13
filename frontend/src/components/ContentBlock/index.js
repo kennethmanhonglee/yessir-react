9c984fea1231630bc69eea11909341a8a1ce7a95
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-09-13 08:50:49 -0700

    added slight styling to distinguish structure

--- frontend/src/components/ContentBlock/index.js
+++ frontend/src/components/ContentBlock/index.js
@@ -1,9 +1,11 @@
+import styles from './ContentBlock.module.css';
+
 const ContentBlock = () => {
     return (
         <>
             {/* general structure, might create individual components later */}
-            <div>Content block buttons</div>
-            <div>Location and address</div>
+            <div className={styles.contentBlockButtons}>Content block buttons</div>
+            <div className={styles.locationAndAddress}>Location and address</div>
             <div>Reviews</div>
         </>
     )
