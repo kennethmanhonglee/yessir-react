8354f19bc4527a4639091bb1f7c67781c860d2cb
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-09-13 15:49:09 -0700

    made controlled input on review form

--- frontend/src/components/ReviewForm/index.js
+++ frontend/src/components/ReviewForm/index.js
@@ -1,22 +1,52 @@
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router";
-import faker from 'faker';
 
 import styles from './ReviewForm.module.css';
+
+const placeholder = 'WOW! This place is so cool, I love their food here...'
 
 const ReviewForm = () => {
     const { businessId } = useParams();
     const businesses = useSelector((state) => state.businesses);
     const currentBusiness = businesses[businessId];
+    const [comment, setComments] = useState('');
+    const [errors, setErrors] = useState([]);
+
+    useEffect(() => {
+        let newErrors = [];
+        if (!comment) errors.push('Comment cannot be empty.');
+
+        setErrors(newErrors);
+
+    }, [comment]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (errors.length === 0) {
+            // post review
+            console.log(comment);
+            setComments('');
+        }
+    }
 
     return (
         <div className={styles.formDiv}>
-            <form className={styles.reviewForm}>
+            <form
+                className={styles.reviewForm}
+                onSubmit={handleSubmit}
+            >
                 {
                     currentBusiness &&
                     <h2>{currentBusiness.title}</h2>
                 }
-                <textArea></textArea>
+                <textarea
+                    placeholder={placeholder}
+                    onChange={(e) => setComments(e.target.value)}
+                    value={comment}
+                ></textarea>
+                <button type='submit' disabled={errors.length > 0} >Post Review</button>
             </form>
         </div>
     )
