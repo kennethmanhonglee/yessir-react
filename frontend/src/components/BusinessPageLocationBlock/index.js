52a528783b793022d863260e5c577c06b16d4cca
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-09-13 14:09:03 -0700

    ran into issue where redux store is not populated upon reload

--- frontend/src/components/BusinessPageLocationBlock/index.js
+++ frontend/src/components/BusinessPageLocationBlock/index.js
@@ -1,9 +1,22 @@
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router';
 import styles from './BusinessPageLocationBlock.module.css'
 
 const BusinessPageLocationBlock = () => {
+    const businesses = useSelector((state) => state.businesses);
+    console.log('1', businesses)
+    const { businessId: currentBusinessId } = useParams();
+    const currentBusiness = businesses[currentBusinessId];
+    console.log('2', currentBusiness)
+    const { address, city, state, zipCode, latitude, longitude } = currentBusiness;
+    console.log('3', address)
     return (
         <div className={styles.locationBlock}>
-            Hello from location block
+            <h2 className={styles.header}>Location</h2>
+            {
+                currentBusiness &&
+                <div>{`${address}, ${city}, ${state} ${zipCode}`}</div>
+            }
         </div>
     );
 }
