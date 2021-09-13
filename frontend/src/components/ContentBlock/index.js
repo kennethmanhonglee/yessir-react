851bc5f74b8da933afdaa6f79b68be50626bf0b2
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-09-12 22:57:27 -0700

    left comment for general direction

--- frontend/src/components/ContentBlock/index.js
+++ frontend/src/components/ContentBlock/index.js
@@ -1,7 +1,7 @@
 const ContentBlock = () => {
     return (
         <>
-
+            {/* general structure, might create individual components later */}
             <div>Content block buttons</div>
             <div>Location and address</div>
             <div>Reviews</div>
