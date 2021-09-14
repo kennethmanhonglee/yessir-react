3e35ccd5ef8a3083cfd50b4180d8ded55357a3b1
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-09-14 10:11:10 -0700

    added checks for creating business

--- backend/routes/api/businesses.js
+++ backend/routes/api/businesses.js
@@ -1,9 +1,40 @@
 const { Business } = require('../../db/models');
 const asyncHandler = require('express-async-handler');
+const { check, validationResult } = require('express-validator');
 
 const express = require('express');
 
 const router = express.Router();
+
+const createBusinessValidations = [
+    check('ownerId')
+        .exists()
+        .withMessage('Must have a ownerID'),
+    check('title')
+        .exists()
+        .withMessage('Must have a title'),
+    check('description')
+        .exists()
+        .withMessage('Must have a description'),
+    check('address')
+        .exists()
+        .withMessage('Must have a address'),
+    check('city')
+        .exists()
+        .withMessage('Must have a city'),
+    check('state')
+        .exists()
+        .withMessage('Must have a state'),
+    check('zipCode')
+        .exists()
+        .withMessage('Must have a zipCode'),
+    check('latitude')
+        .exists()
+        .withMessage('Must have a latitude'),
+    check('longitude')
+        .exists()
+        .withMessage('Must have a longitude'),
+];
 
 router.get('/', asyncHandler(async (req, res) => {
     const response = await Business.findAll();
@@ -11,24 +42,30 @@ router.get('/', asyncHandler(async (req, res) => {
 }));
 
 // need csrf, cant test until implement login
-router.post('/', asyncHandler(async (req, res) => {
+router.post('/', createBusinessValidations, asyncHandler(async (req, res) => {
     const { ownerId, title, description, address, city, state, zipCode, latitude, longitude } = req.body;
 
     //add in validation
+    const validationErrors = validationResult(req);
+    if (validationErrors.isEmpty()) {
+        const newBusiness = await Business.create({
+            ownerId,
+            title,
+            description,
+            address,
+            city,
+            state,
+            zipCode,
+            latitude,
+            longitude
+        });
 
-    const newBusiness = await Business.create({
-        ownerId,
-        title,
-        description,
-        address,
-        city,
-        state,
-        zipCode,
-        latitude,
-        longitude
-    });
-
-    res.json(newBusiness);
+        res.json(newBusiness);
+    } else {
+        // take out errors msgs from validationErrors.errors array
+        const errors = validationErrors.errors.map((error) => error.msg);
+        return res.status(406).json(errors);
+    }
 }));
 
 // probably need csrf too
