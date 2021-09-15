15b4ee191b84e283b0af71f53826a6d3271abad3
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-09-15 15:19:06 -0700

    able to show each comment in p tag in review block

--- frontend/src/components/ReviewsBlock/index.js
+++ frontend/src/components/ReviewsBlock/index.js
@@ -1,11 +1,26 @@
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+
 const ReviewsBlock = () => {
     // on app.js render, store all reviews into redux store as its own slice of state
     // ^done
     // when we load one particular business page, we can go to redux store to get all reviews
-    // ^DOING
+    // ^done
     // and useSelector to FILTER the ones where businessId matches current business.id
+    // ^DOING
+    const { businessId } = useParams();
+
+    const reviewsObj = useSelector((state) => state.reviews);
+    const reviews = Object.values(reviewsObj);
+    const currentReviews = reviews.filter((review) => review.businessId === +businessId);
     return (
-        <div>Hello from reviews block</div>
+        <>
+            {
+                currentReviews.length > 0 ?
+                    currentReviews.map((review) => <p key={review.id}>{review.content}</p>)
+                    : null
+            }
+        </>
     )
 }
 
