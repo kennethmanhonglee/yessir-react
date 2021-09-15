29be82b8fe69a0962754776c78020b1d7471bbb2
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-09-15 14:46:43 -0700

    successfully created seed file for reviews

--- backend/db/seeders/20210915213759-Reviews.js
+++ backend/db/seeders/20210915213759-Reviews.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const faker = require('faker');
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
     /*
@@ -12,6 +14,19 @@ module.exports = {
         isBetaMember: false
       }], {});
     */
+    const fakerReviewsArray = [];
+    for (let i = 0; i < 100; i++) {
+      let amountUsers = 23; //22 faker users + 1 demo user
+      let userId = Math.floor(Math.random() * amountUsers) + 1;
+      let amountBusinesses = 20; //22 faker businesses
+      let businessId = Math.floor(Math.random() * amountBusinesses) + 1;
+      let rating = Math.floor(Math.random() * 5) + 1; //random number from 1-5
+      let content = faker.commerce.productDescription();
+
+      let newReview = { userId, businessId, rating, content };
+      fakerReviewsArray.push(newReview);
+    }
+    return queryInterface.bulkInsert('Reviews', fakerReviewsArray);
   },
 
   down: (queryInterface, Sequelize) => {
@@ -22,5 +37,7 @@ module.exports = {
       Example:
       return queryInterface.bulkDelete('People', null, {});
     */
+
+    return queryInterface.bulkDelete('Reviews', null, { truncate: true, cascade: true, restartIdentity: true });
   }
 };
