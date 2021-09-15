1f6369f95ef175750e0dc805246154b9e1427fb8
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-09-15 14:01:38 -0700

    able to get all reviews from backend api

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -2,11 +2,14 @@ const express = require('express');
 const asyncHandler = require('express-async-handler');
 const { check, validationResult } = require('express-validator');
 
+const { Review } = require('../../db/models');
+
 const router = express.Router();
 
 router.get('/', asyncHandler(async (req, res) => {
     // find all reviews in database
-    res.json('hello from /api/reviews');
+    const reviews = await Review.findAll();
+    res.json(reviews);
 }));
 
 module.exports = router;
