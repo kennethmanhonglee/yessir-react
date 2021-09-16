ebea2388a722bc89554c2d691b5adc4e81e974a4
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-09-16 13:53:08 -0700

    backend api search route can find related entries

--- backend/routes/api/search.js
+++ backend/routes/api/search.js
@@ -1,5 +1,25 @@
 const express = require('express');
+const asyncHandler = require('express-async-handler');
+
+const { Business, Sequelize } = require('../../db/models');
+const Op = Sequelize.Op;
 
 const router = express.Router();
+
+router.post('/', asyncHandler(async (req, res) => {
+    const { searchParamsString, address } = req.body;
+    const searchParams = searchParamsString.split(' ').map((param) => `%${param}%`);
+    const businessesList = await Business.findAll({
+        where: {
+            title: {
+                [Op.iLike]: {
+                    [Op.any]: searchParams
+                }
+            }
+        }
+    });
+
+    res.json(businessesList);
+}));
 
 module.exports = router;
