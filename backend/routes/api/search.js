92357c88c30ed7237609449c836b1a87ecf56887
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-09-16 14:01:03 -0700

    backend api search route can find related entries with search params and address

--- backend/routes/api/search.js
+++ backend/routes/api/search.js
@@ -9,11 +9,36 @@ const router = express.Router();
 router.post('/', asyncHandler(async (req, res) => {
     const { searchParamsString, address } = req.body;
     const searchParams = searchParamsString.split(' ').map((param) => `%${param}%`);
+    const addressParams = address.split(' ').map((param) => `%${param}%`);
     const businessesList = await Business.findAll({
         where: {
-            title: {
-                [Op.iLike]: {
-                    [Op.any]: searchParams
+            [Op.and]: {
+                title: {
+                    [Op.iLike]: {
+                        [Op.any]: searchParams
+                    }
+                },
+                [Op.or]: {
+                    address: {
+                        [Op.iLike]: {
+                            [Op.any]: addressParams
+                        }
+                    },
+                    city: {
+                        [Op.iLike]: {
+                            [Op.any]: addressParams
+                        }
+                    },
+                    state: {
+                        [Op.iLike]: {
+                            [Op.any]: addressParams
+                        }
+                    },
+                    zipCode: {
+                        [Op.iLike]: {
+                            [Op.any]: addressParams
+                        }
+                    },
                 }
             }
         }
