073e7d0f1bcd0e020b6f84424037450a3c1ad22f
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-09-17 10:54:37 -0700

    added slight formatting to footer

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,28 +1,28 @@
 import { Link } from "react-router-dom";
 
-import styles from './Home.module.css';
+import styles from "./Home.module.css";
 import SearchBar from "../SearchBar";
 
-
 const Home = () => {
-    return (
-        <>
-            <div className={styles.main}>
-                <div className={styles.linkIcon}>
-                    <Link to='/'>YESSIR!</Link> { /* placeholder link, change to logo later */}
-                </div>
-                <div className={styles.searchBar}>
-                    {/* make into one component */}
-                    <SearchBar />
-                </div>
-            </div>
-            <div className={styles.footer}>
-                <div className={styles.github}>
-                    <a href='https://github.com/kennethmanhonglee'>GitHub</a>
-                </div>
-            </div>
-        </>
-    );
-}
+  return (
+    <>
+      <div className={styles.main}>
+        <div className={styles.linkIcon}>
+          <Link to="/">YESSIR!</Link>{" "}
+          {/* placeholder link, change to logo later */}
+        </div>
+        <div className={styles.searchBar}>
+          {/* make into one component */}
+          <SearchBar />
+        </div>
+      </div>
+      <div className={styles.footer}>
+        <div className={styles.github}>
+          <a href="https://github.com/kennethmanhonglee">GitHub</a>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Home;
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,47 +1,54 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { AiFillGithub } from "react-icons/ai";
 
-import ProfileButton from './ProfileButton';
-import LoginFormModal from '../LoginFormModal';
-import styles from './Navigation.module.css';
+import ProfileButton from "./ProfileButton";
+import LoginFormModal from "../LoginFormModal";
+import styles from "./Navigation.module.css";
 
 function Navigation({ isLoaded }) {
-    const sessionUser = useSelector(state => state.session.user);
-    let sessionLinks;
-    if (sessionUser) {
-        sessionLinks = (
-            <>
-                <NavLink className={styles.createBusiness} to='/businesses/create'>Create a Business</NavLink>
-                <ProfileButton user={sessionUser} />
-            </>
-        );
-    } else {
-        sessionLinks = (
-            <div className={styles.loginSignup}>
-                <LoginFormModal />
-                <NavLink className={styles.signup} to="/signup">Sign Up</NavLink>
-            </div>
-        );
-    }
-    return (
-        <nav>
-            {/*             
-                add feature later
-                sessionUser &&
-                <li className={styles.reviewBusinesses}>
-                    <NavLink to="/review-page">Write a Review</NavLink>
-                    create a page to suggest businesses for user to write reviews for
-                </li>
-            */}
-            < li className={styles.home} >
-                <NavLink to='/'><div>Yessir Logo</div></NavLink>
-            </li >
-            <li className={styles.functionButtons}>
-                {isLoaded && <div>{sessionLinks}</div>}
-            </li>
-        </nav >
+  const sessionUser = useSelector((state) => state.session.user);
+  let sessionLinks;
+  if (sessionUser) {
+    sessionLinks = (
+      <>
+        <NavLink className={styles.createBusiness} to="/businesses/create">
+          Create a Business
+        </NavLink>
+        <ProfileButton user={sessionUser} />
+      </>
     );
+  } else {
+    sessionLinks = (
+      <div className={styles.loginSignup}>
+        <LoginFormModal />
+        <NavLink className={styles.signup} to="/signup">
+          Sign Up
+        </NavLink>
+      </div>
+    );
+  }
+  return (
+    <nav>
+      {/*             
+            add feature later
+            sessionUser &&
+            <li className={styles.reviewBusinesses}>
+                <NavLink to="/review-page">Write a Review</NavLink>
+                create a page to suggest businesses for user to write reviews for
+            </li>
+        */}
+      <li className={styles.home}>
+        <NavLink to="/">
+          <div>Yessir Logo</div>
+        </NavLink>
+      </li>
+      <li className={styles.functionButtons}>
+        {isLoaded && <div>{sessionLinks}</div>}
+      </li>
+    </nav>
+  );
 }
 
 export default Navigation;
