0406c1d12dac22469babc67a7f13cbc317565bc0
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-09-17 11:26:02 -0700

    styled user logged in modal

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,58 +1,63 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch } from 'react-redux';
-import * as sessionActions from '../../store/session';
-import styles from './ProfileButton.module.css';
+import { useDispatch } from "react-redux";
+import * as sessionActions from "../../store/session";
+import styles from "./ProfileButton.module.css";
 
 function ProfileButton({ user }) {
-    const dispatch = useDispatch();
-    const [showMenu, setShowMenu] = useState(false);
+  const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
 
-    const openMenu = () => {
-        if (showMenu) return;
-        setShowMenu(true);
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = () => {
+      setShowMenu(false);
     };
 
-    useEffect(() => {
-        if (!showMenu) return;
+    document.addEventListener("click", closeMenu);
 
-        const closeMenu = () => {
-            setShowMenu(false);
-        };
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
 
-        document.addEventListener('click', closeMenu);
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout_thunk());
+  };
 
-        return () => document.removeEventListener("click", closeMenu);
-    }, [showMenu]);
-
-    const logout = (e) => {
-        e.preventDefault();
-        dispatch(sessionActions.logout_thunk());
-    };
-
-    return (
-        <div className={styles.loginLogout}>
-            <button className={styles.profileButton} onClick={openMenu}>
-                <i className={`fas fa-user-circle ${styles.icon}`} />
-            </button>
-            {showMenu && (
-                <ul className={`profile-dropdown ${styles.dropdown}`} style={{
-                    backgroundColor: 'white',
-                    border: '1px solid black',
-                    padding: '5px',
-                    margin: '10px',
-                    position: 'absolute',
-                    right: '5%',
-                    top: '10%',
-                }}>
-                    <li>{user.username}</li>
-                    <li>{user.email}</li>
-                    <li>
-                        <button onClick={logout}>Log Out</button>
-                    </li>
-                </ul>
-            )}
+  return (
+    <div className={styles.loginLogout}>
+      <button className={styles.profileButton} onClick={openMenu}>
+        <i className={`fas fa-user-circle ${styles.icon}`} />
+      </button>
+      {showMenu && (
+        <div
+          className={`profile-dropdown ${styles.dropdown}`}
+          style={{
+            backgroundColor: "white",
+            border: "1px solid black",
+            padding: "5px",
+            margin: "10px",
+            position: "absolute",
+            right: "5%",
+            top: "10%",
+          }}
+        >
+          <ul className={styles.ul}>
+            <li>username: {user.username}</li>
+            <li>email: {user.email}</li>
+          </ul>
+          <button className={styles.button} onClick={logout}>
+            Log Out
+          </button>
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default ProfileButton;
