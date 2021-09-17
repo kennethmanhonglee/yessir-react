cefcc85e94db66ef30f5519baaef22d368feb191
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-09-17 11:14:04 -0700

    styled login form

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -1,64 +1,72 @@
-import React, { useState } from 'react';
-import * as sessionActions from '../../store/session';
-import { useDispatch } from 'react-redux';
-import styles from './LoginForm.module.css';
+import React, { useState } from "react";
+import * as sessionActions from "../../store/session";
+import { useDispatch } from "react-redux";
+import styles from "./LoginForm.module.css";
 
 function LoginForm() {
-    const dispatch = useDispatch();
-    const [credential, setCredential] = useState('');
-    const [password, setPassword] = useState('');
-    const [errors, setErrors] = useState([]);
+  const dispatch = useDispatch();
+  const [credential, setCredential] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setErrors([]);
-        return dispatch(sessionActions.login_thunk({ credential, password }))
-            .catch(async (res) => {
-                const data = await res.json();
-                if (data && data.errors) setErrors(data.errors);
-            });
-    }
-
-    const demoUser = () => {
-        setErrors([]);
-        return dispatch(sessionActions.login_thunk({ credential: 'Demo-lition', password: 'password' }))
-            .catch(async (res) => {
-                const data = await res.json();
-                if (data && data.errors) setErrors(data.errors);
-            });
-    }
-
-    return (
-        <div className={styles.formDiv}>
-            <h1>Welcome back.</h1>
-            <form
-                onSubmit={handleSubmit}
-                className={styles.form}
-            >
-                <ul>
-                    {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-                </ul>
-                <input
-                    type="text"
-                    value={credential}
-                    onChange={(e) => setCredential(e.target.value)}
-                    className={styles.input}
-                    required
-                    placeholder='Username or Email'
-                />
-                <input
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                    className={styles.input}
-                    placeholder='Password'
-                />
-                <button className={styles.button} type="submit">Log In</button>
-            </form>
-            <button className={styles.button} onClick={demoUser}>Demo User</button>
-        </div>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors([]);
+    return dispatch(sessionActions.login_thunk({ credential, password })).catch(
+      async (res) => {
+        const data = await res.json();
+        if (data && data.errors) setErrors(data.errors);
+      }
     );
+  };
+
+  const demoUser = () => {
+    setErrors([]);
+    return dispatch(
+      sessionActions.login_thunk({
+        credential: "Demo-lition",
+        password: "password",
+      })
+    ).catch(async (res) => {
+      const data = await res.json();
+      if (data && data.errors) setErrors(data.errors);
+    });
+  };
+
+  return (
+    <div className={styles.formDiv}>
+      <h1>Welcome back.</h1>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <ul>
+          {errors.map((error, idx) => (
+            <li key={idx}>{error}</li>
+          ))}
+        </ul>
+        <input
+          type="text"
+          value={credential}
+          onChange={(e) => setCredential(e.target.value)}
+          className={styles.input}
+          required
+          placeholder="Username or Email"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          className={styles.input}
+          placeholder="Password"
+        />
+        <button className={styles.button} type="submit">
+          Log In
+        </button>
+      </form>
+      <button className={styles.button} onClick={demoUser}>
+        Demo User
+      </button>
+    </div>
+  );
 }
 
 export default LoginForm;
