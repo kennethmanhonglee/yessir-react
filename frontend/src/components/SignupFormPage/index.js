13df8c7c983f7d30a4f5d4201186427f372d332c
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-11-17 11:03:43 -0800

    added food pictures seed data into /seeders

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -4,6 +4,7 @@ import { Redirect, NavLink } from "react-router-dom";
 
 import * as sessionActions from "../../store/session";
 import styles from "./SignupForm.module.css";
+import signup_pic from "../../assets/images/signup_illustration.png";
 
 function SignupFormPage() {
   const dispatch = useDispatch();
@@ -45,61 +46,69 @@ function SignupFormPage() {
   };
 
   return (
-    <div className={styles.formDiv}>
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <h1 className={styles.header}>Create your account.</h1>
-        {errors.length === 0 ? null : (
-          <ul className={styles.errors}>
-            {errors.map((error, idx) => (
-              <li key={idx}>{error}</li>
-            ))}
-          </ul>
-        )}
-        <input
-          placeholder="Email"
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-          className={styles.input}
-        />
-        <input
-          placeholder="Username"
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-          className={styles.input}
-        />
-        <input
-          placeholder="Password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-          className={styles.input}
-        />
-        <input
-          placeholder="Confirm Password"
-          type="password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          required
-          className={styles.input}
-        />
-        <button className={styles.submitButton} type="submit">
-          Sign Up
-        </button>
-        <div className={styles.loginDiv}>
-          <h4>Already a member?</h4>
-          <NavLink to="/login" className={styles.loginLink}>
-            Log in
-          </NavLink>
-        </div>
-        <button className={styles.demo} onClick={demoUser}>
-          Demo User
-        </button>
-      </form>
+    <div className={styles.page_container}>
+      <div className={styles.formDiv}>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <h1>Sign up for Yessir</h1>
+          {errors.length === 0 ? null : (
+            <ul className={styles.errors}>
+              {errors.map((error, idx) => (
+                <li key={idx}>{error}</li>
+              ))}
+            </ul>
+          )}
+          <input
+            placeholder="Email"
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            className={styles.input}
+          />
+          <input
+            placeholder="Username"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            className={styles.input}
+          />
+          <input
+            placeholder="Password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            className={styles.input}
+          />
+          <input
+            placeholder="Confirm Password"
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+            className={styles.input}
+          />
+          <button className={styles.button} type="submit">
+            Sign Up
+          </button>
+          <div className={styles.button} onClick={demoUser}>
+            Demo User
+          </div>
+          <div className={styles.loginDiv}>
+            <h4>Already a member?</h4>
+            <NavLink to="/login" className={styles.loginLink}>
+              Log in
+            </NavLink>
+          </div>
+        </form>
+      </div>
+      <div
+        className={styles.signup_pic}
+        style={{
+          backgroundImage: `url(${signup_pic})`,
+        }}
+      ></div>
     </div>
   );
 }
